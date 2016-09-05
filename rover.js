08fb286a28361e907241cdd6cc793aad30f290e6
Adam Hire <adamdhire@gmail.com>
2016-09-05 10:06:05 -0400

    added index for easier console work as well as a command list property. the rover accepts a string of commands which calls functions

--- rover.js
+++ rover.js
@@ -4,4 +4,21 @@ function Rover(pos, dir) {
   rover = this;
   this.pos = pos;
   this.dir = dir;
+  this.command = function(commands) {
+
+    var commandList = commands.split(', ');
+
+    commandList.forEach(function(command) {
+      if(command === 'f' || command === 'b') {
+        movement(command); // Need to make this function
+      } else if(command === 'r' || command === 'l') {
+        turning(command); // Need to make this function
+      };
+    });
+
+  };
+
+
 };
+
+var wall_E = new Rover([0, 0], 'north');
