23573b4c2d33ade4e68c8215cb30f40d074f2df2
Adam Hire <adamdhire@gmail.com>
2016-09-05 10:11:21 -0400

    forward movement is completed lines 23-43. lines 56-58 tests

--- rover.js
+++ rover.js
@@ -6,19 +6,53 @@ function Rover(pos, dir) {
   this.dir = dir;
   this.command = function(commands) {
 
-    var commandList = commands.split(', ');
+    var commandList = commands.split('');
 
     commandList.forEach(function(command) {
       if(command === 'f' || command === 'b') {
-        movement(command); // Need to make this function
+        movement(command);
       } else if(command === 'r' || command === 'l') {
-        turning(command); // Need to make this function
+        turning(command);
       };
     });
 
   };
 
+  // Movement functionality
+
+  function movement(command) {
+
+    console.log('moving ' + command);
+
+    var xAxis = 0;
+    var yAxis = 0;
+
+    if(rover.dir === 'north') {
+      yAxis  = 1;
+    } else if(rover.dir === 'south') {
+      yAxis = -1;
+    } else if(rover.dir === 'west') {
+      xAxis = -1;
+    } else if(rover.dir === 'east') {
+      xAxis = +1;
+    }
+
+    rover.pos[0] += xAxis;
+    rover.pos[1] += yAxis;
+
+  }
+
+
+  // Turning functionality
+
+  function turning(command) {
+
+    console.log('turning ' + command);
+
+  }
 
 };
 
 var wall_E = new Rover([0, 0], 'north');
+rover.command('ff');
+console.log(wall_E.pos);
